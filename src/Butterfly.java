1ef0450e801d2fac39d45b9465d69319fb0d00f7
shishirwaglek <124612679+shishirwaglek@users.noreply.github.com>
2023-05-17 22:55:36 +0530

    adding files

---
+++ src/Butterfly.java
@@ -0,0 +1,23 @@
+public class Butterfly {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int k=1;k<=2*(n-i);k++)
+                System.out.print(" ");
+            for(int l=1;l<=i;l++)
+                System.out.print("*");
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            for(int k=1;k<=2*(n-i);k++)
+                System.out.print(" ");
+            for(int l=1;l<=i;l++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
